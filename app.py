0ff7db62790a023928c3203769489047b597590c
Jason <jasonapa23@gmail.com>
2025-05-19 20:40:55 +0800

    feat: :sparkles: /api/chat integrated

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return 'Flask backend running'
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    message = data.get('message', '').lower()
+    if message == 'hi':
+        reply = 'hello'
+    else:
+        reply = "I don't understand."
+    return jsonify({"reply": reply})
 
 if __name__ == '__main__':
     app.run(debug=True)
